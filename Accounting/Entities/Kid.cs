db8b5824340e03f9f56984f5f59eee73698ed95c
Sergio <gomez.c.sergio@outlook.com>
2024-12-15 12:35:48 +0100

    Create Kid entity

--- Accounting/Entities/Kid.cs
+++ Accounting/Entities/Kid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,10 @@ namespace Accounting.Entities
 {
     public class Kid
     {
-
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public virtual List<Kid>? Siblings { get; set; }
     }
 }
