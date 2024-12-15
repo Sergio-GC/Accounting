a11b8b5afe5393c4ed8f129933bd8b2f2110ad96
Sergio <gomez.c.sergio@outlook.com>
2024-12-15 12:39:25 +0100

    Create CalendarEntry entity

---
+++ Accounting/Entities/CalendarEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Entities
+{
+    public class CalendarEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Arrival { get; set; }
+        public DateTime Departure { get; set; }
+
+        public virtual List<Kid>? Kids { get; set; }
+    }
+}
