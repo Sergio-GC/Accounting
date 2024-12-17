c8bcc30f13467f682b4445888ce68d776a81924a
Sergio <gomez.c.sergio@outlook.com>
2024-12-17 12:26:08 +0100

    Create EF context and make create demo data

---
+++ Accounting/AccountingContext.cs
@@ -0,0 +1,40 @@
+﻿using Accounting.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Accounting
+{
+    public class AccountingContext : DbContext
+    {
+        public DbSet<Kid> Kids { get; set; }
+        public DbSet<CalendarEntry> CalendarEntries { get; set; }
+
+        // Deliberatedly empty constructor
+        public AccountingContext(){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Kid>()
+                .HasMany(k => k.Siblings)
+                .WithMany(k => k.SiblingOf)
+                .UsingEntity(j => j.ToTable("KidSiblings"));
+
+            modelBuilder.Entity<CalendarEntry>()
+                .HasMany(k => k.Kids)
+                .WithMany(k => k.CalendarEntries)
+                .UsingEntity(j => j.ToTable("CalendarEntryKids"));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            builder.UseLazyLoadingProxies();
+            //builder.UseMySQL(config.GetConnectionString("DefaultDb"));
+            builder.UseMySql(config.GetConnectionString("DefaultDb"), new MySqlServerVersion(new Version()));
+        }
+    }
+}
--- Accounting/Program.cs
+++ Accounting/Program.cs
@@ -1,10 +1,82 @@
-﻿namespace Accounting
+﻿using Accounting.Entities;
+
+namespace Accounting
 {
-    internal class Program
+    public class Program
     {
+        static AccountingContext ctx;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            ctx = new AccountingContext();
+
+            // Initialize db
+            if (ctx.Database.EnsureCreated())
+            {
+                Console.WriteLine("Database has been created!");
+                InitDb();
+            }
+            else
+            {
+                Console.WriteLine("Database exists already"); 
+            }
+
+            Console.WriteLine("Done!");
         }
+
+        #region Database initialization
+        private static void InitDb()
+        {
+            Console.WriteLine("Writing data...");
+
+            Kid Carolina = new Kid() { Name = "Carolina"};
+            Kid Leticia = new Kid() { Name = "Leticia" };
+            Kid Gabi = new Kid() { Name = "Gabi" };
+
+            ctx.Kids.Add(Carolina);
+            ctx.Kids.Add(Leticia);
+            ctx.Kids.Add(Gabi);
+
+            ctx.SaveChanges();
+
+            Kid k = ctx.Kids.Where(k => k.Name == "Carolina").First();
+            Kid i = ctx.Kids.Where(i => i.Name == "Leticia").First();
+
+            if(k.Siblings == null)
+            {
+                k.Siblings = new List<Kid>();
+                k.SiblingOf = new List<Kid>();
+            }
+
+            if(i.Siblings == null)
+            {
+                i.Siblings = new List<Kid>();
+                i.SiblingOf = new List<Kid>();
+
+            }
+
+            k.Siblings.Add(i);
+            k.SiblingOf.Add(i);
+            i.SiblingOf.Add(k);
+            i.Siblings.Add(k);
+
+            ctx.SaveChanges();
+
+            List<Kid> Kids1 = new List<Kid>() { Carolina, Leticia};
+
+            DateTime Date1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 7, 30, 0);
+            DateTime Date11 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 16, 30, 0);
+            DateTime Date2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 30, 0);
+            DateTime Date3 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 7, 15, 0);
+            DateTime Date31 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 18, 45, 0);
+
+            ctx.CalendarEntries.Add(new CalendarEntry() { Kids = Kids1, Date = DateTime.Today, Arrival = Date1, Departure = Date11});
+            ctx.CalendarEntries.Add(new CalendarEntry() { Kids = new List<Kid>() { Carolina }, Date = DateTime.Today.AddDays(1), Arrival = Date2, Departure = Date11 });
+            ctx.CalendarEntries.Add(new CalendarEntry() { Kids = new List<Kid>() { Gabi }, Date = DateTime.Today.AddDays(1), Arrival = Date3, Departure = Date31 });
+
+            ctx.SaveChanges();
+        }
+        #endregion
     }
 }
