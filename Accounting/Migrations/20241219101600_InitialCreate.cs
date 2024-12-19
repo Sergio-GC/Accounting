fb2e04a2e9ea309a52b9d143cfb03ac40774e6ea
Sergio <gomez.c.sergio@outlook.com>
2024-12-19 11:49:54 +0100

    Add db migrations

---
+++ Accounting/Migrations/20241219101600_InitialCreate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounting.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CalendarEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Arrival = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Departure = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarEntries", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Kids",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kids", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CalendarEntryKids",
+                columns: table => new
+                {
+                    CalendarEntriesId = table.Column<int>(type: "int", nullable: false),
+                    KidsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarEntryKids", x => new { x.CalendarEntriesId, x.KidsId });
+                    table.ForeignKey(
+                        name: "FK_CalendarEntryKids_CalendarEntries_CalendarEntriesId",
+                        column: x => x.CalendarEntriesId,
+                        principalTable: "CalendarEntries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CalendarEntryKids_Kids_KidsId",
+                        column: x => x.KidsId,
+                        principalTable: "Kids",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "KidSiblings",
+                columns: table => new
+                {
+                    SiblingOfId = table.Column<int>(type: "int", nullable: false),
+                    SiblingsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KidSiblings", x => new { x.SiblingOfId, x.SiblingsId });
+                    table.ForeignKey(
+                        name: "FK_KidSiblings_Kids_SiblingOfId",
+                        column: x => x.SiblingOfId,
+                        principalTable: "Kids",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_KidSiblings_Kids_SiblingsId",
+                        column: x => x.SiblingsId,
+                        principalTable: "Kids",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalendarEntryKids_KidsId",
+                table: "CalendarEntryKids",
+                column: "KidsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KidSiblings_SiblingsId",
+                table: "KidSiblings",
+                column: "SiblingsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalendarEntryKids");
+
+            migrationBuilder.DropTable(
+                name: "KidSiblings");
+
+            migrationBuilder.DropTable(
+                name: "CalendarEntries");
+
+            migrationBuilder.DropTable(
+                name: "Kids");
+        }
+    }
+}
