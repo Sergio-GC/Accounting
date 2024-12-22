0259a6db6e73e6302792a7a810dcbcf31efbcdf9
Sergio <gomez.c.sergio@outlook.com>
2024-12-22 10:17:52 +0100

    Add BLL KidManager

---
+++ AccountingBLL/IKidManager.cs
@@ -0,0 +1,12 @@
+﻿using Accounting.Entities;
+
+namespace AccountingBLL
+{
+    public interface IKidManager
+    {
+        void AddKid(Kid kid);
+        List<Kid> GetKids();
+        void RemoveKid(Kid kid);
+        void UpdateKid(Kid kid);
+    }
+}
---
+++ AccountingBLL/KidManager.cs
@@ -0,0 +1,41 @@
+﻿using Accounting;
+using Accounting.Entities;
+
+namespace AccountingBLL
+{
+    public class KidManager : IKidManager
+    {
+        private AccountingContext _ctx;
+
+        public KidManager(AccountingContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public List<Kid> GetKids()
+        {
+            return _ctx.Kids.ToList();
+        }
+
+        public void UpdateKid(Kid kid)
+        {
+            Kid OldKid = _ctx.Kids.Where(k => k.Id == kid.Id).Single();
+            OldKid = kid;
+
+            _ctx.SaveChanges();
+        }
+
+        public void AddKid(Kid kid)
+        {
+            _ctx.Kids.Add(kid);
+            _ctx.SaveChanges();
+        }
+
+        public void RemoveKid(Kid kid)
+        {
+            _ctx.Kids.Remove(kid);
+            _ctx.SaveChanges();
+        }
+
+    }
+}
