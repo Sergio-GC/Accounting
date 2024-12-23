75e788ba4663bb03eb8a59e1352e16cc38975c7c
Sergio <gomez.c.sergio@outlook.com>
2024-12-23 12:45:59 +0100

    Create Result Entity for sending total values

---
+++ Accounting/Entities/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace Accounting.Entities
+{
+    public class Result
+    {
+        public DateTime Date {  get; set; }
+        public virtual Kid? Kid { get; set; }
+        public decimal Value { get; set; }
+    }
+}
