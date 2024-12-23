13441160942a3d8d89d74bd6c37ae34b0204b54b
Sergio <gomez.c.sergio@outlook.com>
2024-12-23 12:46:22 +0100

    Finish CalendarEntryManager

--- AccountingBLL/CalendarHandlers/CalendarEntryManager.cs
+++ AccountingBLL/CalendarHandlers/CalendarEntryManager.cs
@@ -1,10 +1,5 @@
 ﻿using Accounting;
 using Accounting.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AccountingBLL.CalendarHandlers
 {
@@ -32,6 +27,14 @@ namespace AccountingBLL.CalendarHandlers
             return _ctx.CalendarEntries.Where(ce => ce.Date < DateTime.Now).ToList();
         }
 
+        public List<CalendarEntry> GetNextEntries()
+        {
+            DateTime StartDate = DateTime.Now.AddDays(-1);
+            DateTime EndDate = DateTime.Now.AddDays(6);
+
+            return _ctx.CalendarEntries.Where(ce => ce.Date > StartDate && ce.Date <= EndDate).ToList();
+        }
+
         public void AddCalendarEntry(CalendarEntry calendarEntry) 
         {
             _ctx.CalendarEntries.Add(calendarEntry);
@@ -50,6 +53,45 @@ namespace AccountingBLL.CalendarHandlers
         {
             _ctx.CalendarEntries.Remove(calendarEntry);
             _ctx.SaveChanges();
+        }
+
+
+        public List<Result> GetResultForMonth(DateTime Month)
+        {
+            // Get the affected entries
+            List<CalendarEntry> ces = _ctx.CalendarEntries.Where(ce => ce.Date.Month.Equals(Month.Month)).ToList();
+
+            // Build the results
+            List<Result> result = new List<Result>();
+            foreach (CalendarEntry ce in ces)
+            {
+                foreach(Kid k in ce.Kids)
+                {
+                    result.Add(new Result { Date = ce.Date, Kid = k, Value = ((decimal)(ce.Departure - ce.Arrival).TotalHours) * ce.Price.Value });
+                }
+            }
+
+            return result;
+        }
+
+        public List<Result> GetResultForKid(Kid kid) 
+        {
+            // Get the affected entries
+            List<CalendarEntry> ces = _ctx.CalendarEntries.Where(ce => ce.Kids.Contains(kid)).ToList();
+
+            List<Result > result = new List<Result>();
+            foreach (CalendarEntry ce in ces) 
+            {
+                foreach (Kid k in ce.Kids)
+                {
+                    if (k.Id != kid.Id)
+                        continue;
+
+                    result.Add(new Result { Date = ce.Date, Kid = k, Value = ((decimal)(ce.Departure - ce.Arrival).TotalHours) * ce.Price.Value });
+                }
+            }
+
+            return result;
         }
     }
 }
